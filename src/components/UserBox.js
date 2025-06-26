b1a6822c9d5be36994002b29695154f484b2cb46
choisungsik <css9596@naver.com>
2025-06-26 17:08:16 +0900

    최신 소스 반영

--- src/components/UserBox.js
+++ src/components/UserBox.js
@@ -6,7 +6,7 @@ function UserBox() {
       <div style={{ color: '#888', fontSize: 14, marginBottom: 8 }}>2025.06.26 THU <span id="js_clock">12:34:56</span></div>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div style={{ marginRight: 16 }}>
-          <img src="https://smart2.dbinc.co.kr/common/encPic.do?ENC_PARAM=c2Rzk7MK0Eyn6sVbyjAiuS5+gOMmRCFkM4U+F1iUlY8AoBz6ZAeVwNtNkVGY2msu3gqQNJzGOQGSG7DAwl8Wb14wjgbO6Q/8BTUActO5x3PdPMNwEV10t5Tp/2Ntqbcq" alt="photo" style={{ width: 80, height: 80, borderRadius: '50%', objectFit: 'cover', border: '2px solid #e5e5e5' }} />
+          <img src="" alt="photo" style={{ width: 80, height: 80, borderRadius: '50%', objectFit: 'cover', border: '2px solid #e5e5e5' }} />
         </div>
         <div>
           <div style={{ fontWeight: 700, fontSize: 18 }}>최성식</div>
