8127bc9229a9f5f31d5c991ba4eadcc254d8ec0b
choisungsik <css9596@naver.com>
2025-06-26 16:04:46 +0900

    최신 소스 반영

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '',
+    publicPath: '/pwaProject/',
   },
   module: {
     rules: [
