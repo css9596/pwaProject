72f31e380083e69c8e118b48ce3b8f3401bf2664
choisungsik <css9596@naver.com>
2025-06-30 18:04:49 +0900

    최신 소스 반영

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// PWA service worker 등록
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      // 등록 성공
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
